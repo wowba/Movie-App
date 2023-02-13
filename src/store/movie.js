26cd0908a2b95aac34210b05c1685fa1d0f85717
wowba <leeyuwk54@gmail.com>
2023-02-14 01:04:08 +0900

    더보기 버튼 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,20 +3,22 @@ import { Store } from "../core/core";
 const store = new Store({
   searchText: '',
   page: 1,
+  pageMax: 1,
   movies: [],
 })
 
 export default store
 export const searchMovies = async page => {
+  store.state.page = page
   if (page === 1) {
-    store.state.page = 1
     store.state.movies = []
   }
 
   const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
-  const { Search } = await res.json()
+  const { Search, totalResults } = await res.json()
   store.state.movies = [
     ...store.state.movies, 
     ...Search
   ]
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10) 
 }
