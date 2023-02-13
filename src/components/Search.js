c799274d064e6775a2f914dc190243c486137914
wowba <leeyuwk54@gmail.com>
2023-02-13 23:53:55 +0900

    검색 API 연결

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import { Component } from "../core/core";
+import movieStore, { searchMovies } from '../store/movie'
 
 export default class Search extends Component {
   render() {
@@ -13,18 +14,18 @@ export default class Search extends Component {
 
   const inputEl = this.el.querySelector('input')
   inputEl.addEventListener('input', () => {
-    //
+    movieStore.state.searchText = inputEl.value
   })
   inputEl.addEventListener('keydown', event => {
-    if (event.key === 'Enter') {
-      //
+    if (event.key === 'Enter' && movieStore.state.searchText.trim()) {
+      searchMovies(1)
     }
   })
 
   const btnEl = this.el.querySelector('.btn')
   btnEl.addEventListener('click', () => {
     if (movieStore.state.searchText.trim()) {
-      //
+      searchMovies(1)
     }
   })
   }
