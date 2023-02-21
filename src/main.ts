f65af136dce62e03b3505bea5260eec5df1e9176
wowba <leeyuwk54@gmail.com>
2023-02-22 01:57:48 +0900

    타입스크립트 프로젝트 세팅, core.ts 파일 수정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import router from './routes/index'
 
 // index.html 파일의 div#root
 const root = document.querySelector("#root")
-root.append(new App().el)
+// 선택적 체이닝 사용
+root?.append(new App().el)
 
 router()
