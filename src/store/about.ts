67c447e0c64f3dcdb89565d426cd4811831d83cc
wowba <leeyuwk54@gmail.com>
2023-02-22 21:56:20 +0900

    스토어 컴포넌트 변환

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,15 @@
 import { Store } from '../core/core'
 
-export default new Store({
+interface State {
+  photo: string
+  name: string
+  email: string
+  blog: string
+  github: string
+  repository: string
+}
+
+export default new Store<State>({
   photo: 'https://heropy.blog/css/images/logo.png',
   name: 'WOWBA / Lee YeongUk',
   email: 'leeyuwk54@gmail.com',
